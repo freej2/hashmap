7e7e09286c1934bf194674b77a2b5c672a3f7f1e
Jacob <40305268+freej2@users.noreply.github.com>
2025-01-19 16:32:53 -0800

    added main.js and functionality of HashMap

--- hashmap.js
+++ hashmap.js
@@ -1,11 +1,12 @@
 import LinkedList from "./linkedList.js";
 
-class HashMap {
+export default class HashMap {
     constructor(capacity = 16, load_factor = 0.75) {
         this.capacity = capacity;
         this.load_factor = load_factor;
         // Create array of buckets, each bucket will hold a LinkedList
         this.buckets = new Array(capacity).fill(null);
+        this.size = 0;
     }
 
     hash(key) {
@@ -25,6 +26,35 @@ class HashMap {
         if (!this.buckets[index]) {
             this.buckets[index] = new LinkedList();
         }
+
+        if (this.size > this.capacity * this.load_factor){
+            // Double the capacity
+            this.capacity *= 2;
+            
+            // Store old buckets
+            const oldBuckets = this.buckets;
+            
+            // Create new array with doubled size
+            this.buckets = new Array(this.capacity).fill(null);
+            
+            // Rehash all existing entries
+            for (let i = 0; i < oldBuckets.length; i++) {
+                if (oldBuckets[i]) {
+                    let currentNode = oldBuckets[i].getHead();
+                    while (currentNode) {
+                        // Rehash and insert each key-value pair into new buckets
+                        const newIndex = this.hash(currentNode.key);
+                        if (!this.buckets[newIndex]) {
+                            this.buckets[newIndex] = new LinkedList();
+                        }
+                        this.buckets[newIndex].append(currentNode.key, currentNode.value);
+                        currentNode = currentNode.nextNode;
+                    }
+                }
+            }
+        }
+
+        
         // Let the LinkedList handle storing/updating the key-value pair
         this.buckets[index].upsert(key, value);
     }
@@ -40,32 +70,84 @@ class HashMap {
         return node ? node.value : null;
     }
 
-    has(key){
-
+    has(key) {
+        const index = this.hash(key);
+        if (!this.buckets[index]) {
+            return false;
+        }
+        // Check if key exists in the bucket
+        return this.buckets[index].findByKey(key) !== null;
     }
 
-    remove(key){
-
+    remove(key) {
+        const index = this.hash(key);
+        if (this.has(key)) {
+            this.buckets[index].removeByKey(key);  // Pass the key!
+            this.size--;
+            return true;
+        }
+        return false;
     }
 
-    length(){
-
+    length() {
+        let count = 0;
+        for (let i = 0; i < this.capacity; i++) {
+            if (this.buckets[i]) {
+                count += this.buckets[i].size();
+            }
+        }
+        return count;
     }
 
     clear(){
-
+        for(let i = 0; i < this.capacity; i++){
+            this.buckets[i] = null;
+        }
+        this.size = 0;
     }
 
-    keys(){
-
+    keys() {
+        const keys = [];
+        for (let i = 0; i < this.capacity; i++) {
+            if (this.buckets[i]) {
+                // Start at the head of the LinkedList
+                let currentNode = this.buckets[i].head;
+                // Traverse the LinkedList
+                while (currentNode !== null) {
+                    keys.push(currentNode.key);
+                    currentNode = currentNode.nextNode;
+                }
+            }
+        }
+        return keys;
     }
 
-    values(){
-
+    values() {
+        const values = [];
+        for (let i = 0; i < this.capacity; i++) {
+            if (this.buckets[i]) {
+                let currentNode = this.buckets[i].head;
+                while (currentNode !== null) {
+                    values.push(currentNode.value);
+                    currentNode = currentNode.nextNode;
+                }
+            }
+        }
+        return values;
     }
 
-    entries(){
-
+    entries() {
+        const entries = [];
+        for (let i = 0; i < this.capacity; i++) {
+            if (this.buckets[i]) {
+                let currentNode = this.buckets[i].head;
+                while (currentNode !== null) {
+                    entries.push([currentNode.key, currentNode.value]);
+                    currentNode = currentNode.nextNode;
+                }
+            }
+        }
+        return entries;
     }
       
 }
